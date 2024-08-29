1789ee6d9baffb00a5a80caffa017cfe292286f8
Shaun <shaunmadziva@gmail.com>
2024-08-29 11:33:32 +0100

    palindrome function

---
+++ app.js
@@ -0,0 +1,14 @@
+function isPalindrome(string) {
+  const list = string.split('');
+  //console.log(list);
+  const reversed = list.toReversed();
+  //console.log(reversed);
+  const string2 = reversed.join("")
+  //console.log(string2);
+  if (string == string2) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isPalindrome("racecar"));
