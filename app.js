93c2d882d61cae422b607c8229bdc2caa70e3b24
Shaun <shaunmadziva@gmail.com>
2024-08-29 12:00:57 +0100

    unit test for isPalindrome

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function isPalindrome(string) {
-  const list = string.split('');
+export function isPalindrome(string) {
+  const list = string.split("");
   //console.log(list);
   const reversed = list.toReversed();
   //console.log(reversed);
-  const string2 = reversed.join("")
+  const string2 = reversed.join("");
   //console.log(string2);
   if (string == string2) {
     return true;
